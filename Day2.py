742325aed4c133fb4b7ea74cac6fb205a22de0e8
DineshK-18 <dineshkaruppusamy146@gmail.com>
2025-04-12 09:41:30 +0530

    Day2

---
+++ Day2.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+bill=float(input("What was the total bill?"))
+tip=int(input("How much tip would you like to give?"))
+act_tip=tip/100
+people=int(input("How many people to split the bill?"))
+total_bill=((bill*act_tip+bill)/people)
+print("Each Person should pay:",round(total_bill,2))
