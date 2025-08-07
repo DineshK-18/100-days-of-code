936a9d0fc45642a33927bce9a6fd7f023af27284
DineshK-18 <dineshkaruppusamy146@gmail.com>
2025-08-07 21:49:16 +0530

    Day16

---
+++ Day16/Main.py
@@ -0,0 +1,21 @@
+from menu import Menu,MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+should_continue = True
+while should_continue:
+    user_need = input(f'What would you like?({menu.get_items()}):')
+    if user_need == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif user_need == 'off':
+        should_continue = False
+    else:
+        drink = menu.find_drink(user_need)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
