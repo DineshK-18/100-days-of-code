71b046980234eda7a0c0b07a11d9673aaa12f3a7
DineshK-18 <dineshkaruppusamy146@gmail.com>
2025-09-01 23:27:08 +0530

    Day18

--- Day18/main.py
+++ Day18/main.py
@@ -7,8 +7,8 @@ rgb_colors = []
 colors = colorgram.extract('image.jpg',100)
 for color in colors:
     r = color.rgb.r
-    g = color.rgb.r
-    b = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
     new_color = (r,g,b)
     rgb_colors.append(new_color)
 print(rgb_colors)
