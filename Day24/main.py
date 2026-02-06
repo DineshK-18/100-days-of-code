caa906b39325c771e148f77a6f99868b66efb6a4
DineshK-18 <dineshkaruppusamy146@gmail.com>
2026-02-06 12:32:27 +0530

    Day24

---
+++ Day24/main.py
@@ -0,0 +1,8 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# # print(contents)
+
+with open("new_file.txt", "a") as file:
+    # file.write("\nI am 50 years old.")
+    # file.write("\nI Love my family.")
+    file.write("I will do anything to see my family happy.")
