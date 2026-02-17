3b2a226ae4418603009077a061fb4bc23b2475fe
DineshK-18 <dineshkaruppusamy146@gmail.com>
2026-02-17 12:52:11 +0530

    Day25

---
+++ Day25/main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# max_temp = data['temp'].max()
+#
+# print(data[data.temp == data.temp.max()])
+
+# °F = (°C × 1.8) + 32
+
+# new_data = data[data.day == "Monday"]
+# val = new_data.temp
+# print((val*1.8) + 32)
+#
+
+#
+# data = pd.DataFrame(data_dict)
+# print(data)
+#
+# data.to_csv("new_data.csv")
+
+
+#
+# grey_squirrel = len(data[data["Primary Fur Color"] == "Gray"])
+# Cinnamon_squirrel = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# Black_squirrel = len(data[data["Primary Fur Color"] == "Black"])
+#
+# data_dict = {
+#     "Fur Color": ["Gray", "Cinnamon", "Black"],
+#     "Count": [grey_squirrel, Cinnamon_squirrel, Black_squirrel]
+# }
+#
+# df = pd.DataFrame(data_dict)
+# df.to_csv("squirrel_fur_color.csv")
+
+# data = pd.read_csv("weather_data.csv")
+# print(data[data.temp == data.temp.max()])
+
+
+
+
+
+
